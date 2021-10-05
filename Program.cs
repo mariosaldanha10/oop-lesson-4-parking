9b59e25f8a63f10161f9e9a85db12425a3895742
Mario <Mario@MARIO>
2021-10-05 14:53:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lesson_4_parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
